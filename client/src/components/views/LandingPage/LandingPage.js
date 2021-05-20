b628c096c40f86e955719348913aa3ef41181dce
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-20 19:02:57 +0900

    시작 화면 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { FaCode } from "react-icons/fa";
+import {Typography,Row,Col,Button} from 'antd'
+
+const {Title}= Typography
 
 function LandingPage() {
     return (
-        <>
-            <div className="app">
-                <FaCode style={{ fontSize: '4rem' }} /><br />
-                <span style={{ fontSize: '2rem' }}>Let's Start Coding!</span>
-            </div>
-            <div style={{ float: 'right' }}>Thanks For Using This Boiler Plate by John Ahn</div>
-        </>
+        <div style={{width:'85%', margin:'6rem auto', display:'flex', alignItems:'center', justifyContent:'center', flexDirection:'column'}}>
+            <Title level={2}>1 Day 1 Comment</Title>      
+            <br />
+            <br />
+            <Row style={{width:'30rem', marginLeft:'16.5rem', marginBottom:'1rem'}}>
+                <Col lg={6} md={8} xs={24}>
+                    <a href={`/comment/${1}`}><Button style={{backgroundColor:'grey', color:'white', width:'6rem', height:'2.5rem'}}>명언</Button></a>
+                </Col>
+                <Col lg={6} md={8} xs={24}>
+                <a href={`/comment/${1}`}><Button style={{backgroundColor:'grey', color:'white', width:'6rem', height:'2.5rem'}}>바이블</Button></a>
+                </Col>
+            </Row>    
+            <br />
+            <Row style={{width:'30rem', marginLeft:'16.5rem'}}>
+                <Col lg={6} md={8} xs={24}>
+                <a href={`/comment/${1}`}><Button style={{backgroundColor:'grey', color:'white', width:'6rem', height:'2.5rem'}}>영문장</Button></a>
+                </Col>
+                <Col lg={6} md={8} xs={24}>
+                <a href={`/comment/${1}`}><Button style={{backgroundColor:'grey', color:'white', width:'6rem', height:'2.5rem'}}>사자성어</Button></a>
+                </Col>
+            </Row>
+        </div>
     )
 }
 
