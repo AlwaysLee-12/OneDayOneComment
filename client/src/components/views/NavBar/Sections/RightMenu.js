564d0e941c207a773483f2a7dc8ebf0da97f1170
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-05-20 19:11:44 +0900

    네비게이션 바 및 푸터 수정

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -36,6 +36,9 @@ function RightMenu(props) {
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
+        <Menu.Item key="myRecorded">
+          <a href="/myRecorded">myRecorded</a>
+        </Menu.Item>
       </Menu>
     )
   }
