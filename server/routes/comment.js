278569a2b03eb0b8ba429d3d96fe23ec0e1e2337
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-06-02 12:07:37 +0900

    Feat: Implement Processing Exception in AddComment

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -10,12 +10,16 @@ const { auth } = require("../middleware/auth");
 
 router.post("/uploadComment", auth,(req, res) => {
 
-    const comment= new Comment(req.body)
-
-    comment.save((err,doc)=>{
-        if(err) return res.status(400).json({success:false, err})
-        return res.status(200).json({success:true})
-    })
+    Comment.findOne({title:req.body.title})
+        .exec((err,user)=>{
+            if(err) return res.status(400).json({success:false, err})
+            else if(user) return res.json({success:false, message: 'Your Comment is Already Exist! Try Another One'})
+            const comment= new Comment(req.body)
+            comment.save((err,doc)=>{
+                if(err) return res.status(400).json({success:false, err})
+                return res.status(200).json({success:true})
+            })
+        })
 });
 
 module.exports = router;
