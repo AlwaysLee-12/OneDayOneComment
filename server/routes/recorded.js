1d291ac95f8a58ce1444986820b3efbac6b47a86
AlwaysLee-Creater <54054460+AlwaysLee-Creater@users.noreply.github.com>
2021-06-21 13:06:12 +0900

    Refactor: Separate into controller and route

--- server/routes/recorded.js
+++ server/routes/recorded.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { Comment } = require("../models/Comment");
-const {Recorded}= require("../models/Recorded");
 const { auth } = require("../middleware/auth");
+const recordedController= require('../controllers/recorded')
 
 //=================================
 //             Recorded
 //=================================
 
-router.post("/getRecorded", auth,(req, res) => {
-    Recorded.find({"userInfo":req.body.userInfo})
-        .populate('userInfo')
-        .exec((err,results)=>{
-            if(err) return res.status(400).json({success:false, err})
-            return res.status(200).json({success:true, comments:results})
-        })
-});
+router.post("/getRecorded", auth, recordedController.getRecorded);
 
 module.exports = router;
